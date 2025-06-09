0c80c4bd732e0bee488a165edf722869e094dedf
lakshmanana456 <elakshmanan10501@gmail.com>
2025-06-09 22:06:46 +0530

    ui update

--- script.js
+++ script.js
@@ -1,43 +1,64 @@
- var signup = document.querySelector(".signup")
-    signup.addEventListener("click", function () {
+ var submit = document.querySelector(".submit")
+    submit.addEventListener("click", function () {
       event.preventDefault()
-      
-
 
       var nameRegex = /^[a-zA-Z]+$/
       var emailRegex = /^[a-zA-Z0-9]+@gmail.com/
+      var messageRegex = /^(?:\b\w+\b[\s\r\n]*){1,200}$/
+
+
+      // var passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*\w).{8,}$/
 
       // 
       var name = document.querySelector(".name")
       var email = document.querySelector(".email")
       var emailError = document.querySelector(".emailError")
-      var nameError=document.querySelector(".nameError")
+      var nameError = document.querySelector(".nameError")
+      var message=document.querySelector(".message")
+      var messageError=document.querySelector(".messageError")
 
 
-      var valid = true 
+      var valid = true //check it all condition true or false
 
-      if (nameRegex.test(name.value) == false) { 
+      if (nameRegex.test(name.value) == false) { //use test to compare values
         var valid = false
 
-        nameError.style.display = "inline"
+        nameError.style.display = "block"
       } else {
         nameError.style.display = "none"
       }
 
-       if (emailRegex.test(email.value) == false) { 
+
+      if (emailRegex.test(email.value) == false) { //use test to compare values
         var valid = false
 
-        emailError.style.display = "inline"
+        emailError.style.display = "block"
       } else {
         emailError.style.display = "none"
       }
 
+      if (messageRegex.test(message.value) == false) { //use test to compare values
+        var valid = false
 
-     
+        messageError.style.display = "block"
+      } else {
+        messageError.style.display = "none"
+      }
+
+      // if (passwordRegex.test(password.value) == false) { //use test to compare values
+      //   var valid = false
+
+      //   passwordError.style.display = "inline"
+      // } else {
+      //   passwordError.style.display = "none"
+      // }
+
+
       if (valid == true) {
         alert("Message send successfully")
-         
+
 
       }
 
     })
+
