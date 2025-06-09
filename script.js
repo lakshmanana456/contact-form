e4c46c437700fe76e04a050a9b1537dab499fc28
lakshmanana456 <elakshmanan10501@gmail.com>
2025-06-09 22:12:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,64 +1,64 @@
- var submit = document.querySelector(".submit")
-    submit.addEventListener("click", function () {
-      event.preventDefault()
+var submit = document.querySelector(".submit")
+submit.addEventListener("click", function () {
+  event.preventDefault()
 
-      var nameRegex = /^[a-zA-Z]+$/
-      var emailRegex = /^[a-zA-Z0-9]+@gmail.com/
-      var messageRegex = /^(?:\b\w+\b[\s\r\n]*){1,200}$/
-
-
-      // var passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*\w).{8,}$/
-
-      // 
-      var name = document.querySelector(".name")
-      var email = document.querySelector(".email")
-      var emailError = document.querySelector(".emailError")
-      var nameError = document.querySelector(".nameError")
-      var message=document.querySelector(".message")
-      var messageError=document.querySelector(".messageError")
+  var nameRegex = /^[a-zA-Z]+$/
+  var emailRegex = /^[a-zA-Z0-9]+@gmail.com/
+  var messageRegex = /^(?:\b\w+\b[\s\r\n]*){1,200}$/
 
 
-      var valid = true //check it all condition true or false
+  // var passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*\w).{8,}$/
 
-      if (nameRegex.test(name.value) == false) { //use test to compare values
-        var valid = false
-
-        nameError.style.display = "block"
-      } else {
-        nameError.style.display = "none"
-      }
-
-
-      if (emailRegex.test(email.value) == false) { //use test to compare values
-        var valid = false
-
-        emailError.style.display = "block"
-      } else {
-        emailError.style.display = "none"
-      }
-
-      if (messageRegex.test(message.value) == false) { //use test to compare values
-        var valid = false
-
-        messageError.style.display = "block"
-      } else {
-        messageError.style.display = "none"
-      }
-
-      // if (passwordRegex.test(password.value) == false) { //use test to compare values
-      //   var valid = false
-
-      //   passwordError.style.display = "inline"
-      // } else {
-      //   passwordError.style.display = "none"
-      // }
+  // 
+  var name = document.querySelector(".name")
+  var email = document.querySelector(".email")
+  var emailError = document.querySelector(".emailError")
+  var nameError = document.querySelector(".nameError")
+  var message = document.querySelector(".message")
+  var messageError = document.querySelector(".messageError")
 
 
-      if (valid == true) {
-        alert("Message send successfully")
+  var valid = true //check it all condition true or false
+
+  if (nameRegex.test(name.value) == false) { //use test to compare values
+    var valid = false
+
+    nameError.style.display = "block"
+  } else {
+    nameError.style.display = "none"
+  }
 
 
-      }
+  if (emailRegex.test(email.value) == false) { //use test to compare values
+    var valid = false
 
-    })
+    emailError.style.display = "block"
+  } else {
+    emailError.style.display = "none"
+  }
+
+  if (messageRegex.test(message.value) == false) { //use test to compare values
+    var valid = false
+
+    messageError.style.display = "block"
+  } else {
+    messageError.style.display = "none"
+  }
+
+  // if (passwordRegex.test(password.value) == false) { //use test to compare values
+  //   var valid = false
+
+  //   passwordError.style.display = "inline"
+  // } else {
+  //   passwordError.style.display = "none"
+  // }
+
+
+  if (valid == true) {
+    alert("Message send successfully")
+
+
+  }
+
+})
 
